215c8ff39a192946592fee72fa3e453c0ad1ca31
Kyle Wilson <>
2026-06-26 14:31:24 -0400

    setting up sending emails

--- components/ContactForm/index.vue
+++ components/ContactForm/index.vue
@@ -1,29 +1,113 @@
 <template>
     <div class="kw-contact-form">
         <div class="kw-contact-form__inner">
-            <form>
+            <form ref="contactForm">
                 <div class="kw-contact-form__form-content">
                     <div class="kw-contact-form__row">
-                        <input type="text" placeholder="First name">
-                        <input type="text" placeholder="Last name">
+                        <input 
+                            v-model="formFields.firstName" 
+                            required 
+                            type="text" 
+                            placeholder="First name *"
+                            :disabled="isLoading"
+                        >
+                        <input 
+                            v-model="formFields.lastName" 
+                            required 
+                            type="text" 
+                            placeholder="Last name *"
+                            :disabled="isLoading"
+                        >
                     </div>
                     
                     <div class="kw-contact-form__row">
-                        <input type="email" placeholder="Email">
+                        <input 
+                            v-model="formFields.email" 
+                            required 
+                            type="email" 
+                            placeholder="Email *"
+                            :disabled="isLoading"
+                        >
                     </div>
 
                     <div class="kw-contact-form__row">
-                        <textarea placeholder="Tell us about your project"></textarea>
+                        <textarea 
+                            v-model="formFields.details" 
+                            required 
+                            placeholder="Tell us about your project *"
+                            :disabled="isLoading"
+                        ></textarea>
                     </div>
 
-                    <div class="kw-contact-form__row">
-                        <button class="kw-button --button4">Submit</button>
+                    <div class="kw-contact-form__row">                        
+                        <button 
+                            @click.prevent="sendMail" 
+                            class="kw-button --button4"
+                            :disabled="disableSubmit"
+                            type="submit"
+                        >
+                            Submit
+                        </button>
+                    </div>
+
+                    <div v-if="responseMessage" class="kw-contact-form__row --message">
+                        <p>{{ responseMessage }}</p>
                     </div>
                 </div>            
             </form>
         </div>
     </div>
 </template>
+
+<script setup>
+const formFields = ref({
+    firstName: '',
+    lastName: '',
+    email: '',
+    details: ''
+})
+
+const isLoading = ref(false)
+const responseMessage = ref('')
+const contactForm = ref()
+
+const disableSubmit = computed(() => {        
+    return !isLoading.value &&
+            !formFields.value.firstName &&
+            !formFields.value.lastName &&
+            !formFields.value.email &&
+            !formFields.value.details            
+})
+
+const sendMail = async () => {    
+    if(!contactForm?.value.checkValidity()) return 
+
+    try {
+        isLoading.value = true
+
+        const response = await $fetch('/api/send-email', {
+            method: 'POST',
+            body: {
+                firstName: formFields.value.firstName,
+                lastName: formFields.value.lastName,        
+                details: formFields.value.details,
+                email: formFields.value.email
+            },
+        })    
+
+        responseMessage.value = response.statusMessage
+        isLoading.value = false
+
+    } catch (error) {    
+        responseMessage.value = error.statusMessage    
+    } finally {
+        formFields.firstName = ''
+        formFields.lastName = ''
+        formFields.email = ''
+        formFields.details = ''
+    }
+}
+</script>
 
 <style lang="scss" scoped>
 .kw-contact-form {
@@ -43,6 +127,19 @@
         button {
             width: 100%;
             cursor: pointer;
+        }
+
+        &.--message {
+            text-align: center;
+
+            p {
+                width: 100%;
+                color: $color3;
+                margin: 0px;
+                padding: rem(8) rem(16);
+                background-color: $color2;   
+                border-radius: rem(20);             
+            }
         }
     }
 }
---
+++ server/api/send-email.js
@@ -0,0 +1,28 @@
+import { useSendEmail } from '@/composables/useSendEmail'
+
+export default defineEventHandler(async (event) => {
+    const body = await readBody(event)    
+    const { formatEmail } = useSendEmail()
+             
+    try {
+        await formatEmail({
+            firstName: body.firstName,
+            lastName: body.lastName,
+            email: body.email,
+            details: body.details
+        })
+        
+        return { 
+            statusCode: 200,
+            success: true,
+            statusMessage: 'Email was sent successfully. We will get back to you as soon as possible.'
+        }
+    } catch (error) {        
+        throw createError({
+            statusCode: 500,
+            success: false,
+            statusMessage: 'Failed to send email',
+            errorMessage: error.message
+        })
+    }
+})
